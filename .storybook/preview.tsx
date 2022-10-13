351ae80e44871dcca47f7f2cd24bc2633c7373dc
hamdij4 <radoncic.hamdija@gmail.com>
2022-10-13 13:11:09 +0200

    git commit -m "D2-97 <message>"

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components'
 import { DecoratorFn } from '@storybook/react'
 import { darkTheme, lightTheme } from '../src/themes/themes'
+import styled, { css } from 'styled-components'
 import React from 'react'
 
 export const parameters = {
@@ -13,14 +14,24 @@ export const parameters = {
     },
   },
 }
+const ThemeBlock = styled.div<{ left?: boolean; fill?: boolean }>(
+  ({ theme }) =>
+    css`
+      background: ${theme.name === 'dark' ? theme.colors.tuna : theme.colors.white};
+      border-radius: 5px;
+      padding: 24px;
+    `
+)
+
 
 const withTheme: DecoratorFn = (StoryFn, context) => {
-  // Get the active theme value from the story parameter
-  const theme = context.parameters.theme || context.globals.theme
-  const storyTheme = theme === 'dark' ? darkTheme : lightTheme
+  const theme = context.parameters.theme || context.globals.theme;
+  const storyTheme = theme === 'dark' ? darkTheme : lightTheme;
   return (
     <ThemeProvider theme={storyTheme}>
-      <StoryFn />
+      <ThemeBlock fill>
+        <StoryFn />
+      </ThemeBlock>
     </ThemeProvider>
   )
 }
@@ -31,19 +42,14 @@ export const globalTypes = {
     description: 'Global theme for components',
     defaultValue: 'light',
     toolbar: {
-      // The icon for the toolbar item
       icon: 'circlehollow',
-      // Array of options
       items: [
         { value: 'light', icon: 'circlehollow', title: 'light' },
         { value: 'dark', icon: 'circle', title: 'dark' },
       ],
-      // Property that specifies if the name of the item will be displayed
       showName: true,
     },
   },
 }
 
-
-// export all decorators that should be globally applied in an array
 export const decorators = [withTheme]
