950a05f4ffed32ffa908455bb779de5b01bdb2e1
hamdij4 <radoncic.hamdija@gmail.com>
2025-02-04 10:39:50 +0100

    0.7.71 -> Fixing dropdown styles

--- src/components/Switch/Switch.stories.tsx
+++ src/components/Switch/Switch.stories.tsx
@@ -3,16 +3,21 @@ import { Meta, Story } from "@storybook/react/types-6-0";
 import Switch, { SwitchProps } from "./Switch";
 import Icon from "../Icons/Icons";
 
-const text = (a) => console.log(a)
+const text = (a) => console.log(a);
 export default {
   title: "Components/Switch",
   component: Switch,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 } as Meta;
-const Template: Story<SwitchProps> = (args) => <Switch {...args} />;
+const Template: Story<SwitchProps> = (args) => <><Switch {...args} /><Switch {...args} checked={false} /></>;
 
 export const Primary = Template.bind({});
-Primary.args = { label: "Switch", checked: true, labelPosition: 'top', variant: 'secondary',
-onChange: text};
+Primary.args = {
+  label: "Switch",
+  checked: true,
+  labelPosition: "top",
+  variant: "primary",
+  onChange: text,
+};
